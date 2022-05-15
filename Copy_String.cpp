5d391dffc194083a199d088defc34aeb75c0ee61
Kiarash Siahbandi <kiarashsiahbandi@gmail.com>
2022-05-15 14:43:50 +0430

    Add files via upload
    This program will get a string from user and make a copy of it.
    Simple and understandable.(C++)

---
+++ Copy_String.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+	string s1, s2;
+	cout << "Enter string S1: ";
+	getline(cin, s1);
+	s2=s1;
+	cout << "S1 =	"<<s1<< endl;
+	cout << "S2 =	"<<s2;
+	return 0;
+}
